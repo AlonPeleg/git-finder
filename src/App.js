3e74408fddbd2919a5fde9a59def7010bb433923
Alon Peleg <peleg619@gmail.com>
2019-07-27 05:46:04 +0300

    Alert incase search empty
    added alert for 5 sec if search is empty string.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // Search users and add them to state
@@ -26,16 +28,26 @@ class App extends Component {
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
