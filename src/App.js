c6ebba9d08f35959de4cec6482deddc9412935ca
AlonPeleg <peleg619@gmail.com>
2019-07-28 11:50:36 +0300

    converting Search to Context
    horrible.

--- src/App.js
+++ src/App.js
@@ -19,19 +19,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search users and add them to state
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get a single Github user
   const getUser = async username => {
     setLoading(true);
@@ -84,7 +71,6 @@ const App = () => {
                 render={() => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length ? true : false}
                       setAlert={showAlert}
